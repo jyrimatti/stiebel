b3e8b3daf82f54bf41d9ca5bc275eb8609b422f1
Jyri-Matti Lahteenmaki <jyri-matti@lahteenmaki.net>
2019-02-10 15:26:30 +0000

    archiving

---
+++ archive.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p bash
+set -eu
+
+year=$1
+millis=$2
+
+
+cd data
+mkdir $year
+ls *.txt | xargs -I [] sh -c "cat [] | cut -d , -f 1 | cut -d [ -f 2 | awk '\$1 < $millis' | wc -l | awk '{if (\$1 > 0) {print \$1-1} else {print 0}}' | xargs -I {} sh -c 'head -{} [] > $year/[] && sed -i \"1,+{}d\" []'"
+cd ..
+
+# 2015 1451599200000
+# 2016 1483221600000
+# 2017 1514757600000
+# 2018 1546293600000
