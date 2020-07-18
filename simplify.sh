c85b0c359086d5b2b47b2c4c838d58066abcb68b
Jyri-Matti Lahteenmaki <jyri-matti@lahteenmaki.net>
2020-07-18 08:57:36 +0000

    fixes

--- simplify.sh
+++ simplify.sh
@@ -6,6 +6,6 @@ DIR=$1
 
 for f in $DIR/*.txt; do
   mv $f "$f.lock"
-  uniq --skip-fields=1 < "$f.lock" > $f
+  (tr -cd '\11\12\15\40-\176' | uniq --skip-fields=1) < "$f.lock" > $f
   rm "$f.lock"
 done
