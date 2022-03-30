fd2251ea82471307b577a395a873e0541a52d116
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2022-03-30 20:18:46 +0300

    delay

--- ouman_subscribe.sh
+++ ouman_subscribe.sh
@@ -10,7 +10,7 @@ source ./ouman_objects.sh "$1"
 
 WSTOKEN=$(curl -s "https://oulite.ouman.io/socket.io/1/?deviceid=$DEVICEID&token=$TOKEN" | sed 's/\([^:]*\):.*/\1/g')
 
-echo '5:::{"name":"message","args":["{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"subscribe\",\"params\":{\"objects\":[{\"id\":\"'"$OBJECTID"'\",\"device\":255,\"properties\":{\"85\":{}}}]}}"]}' |
+(echo -n; sleep 1; echo '5:::{"name":"message","args":["{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"subscribe\",\"params\":{\"objects\":[{\"id\":\"'"$OBJECTID"'\",\"device\":255,\"properties\":{\"85\":{}}}]}}"]}') |
   websocat -n "wss://oulite.ouman.io/socket.io/1/websocket/$WSTOKEN?deviceid=$DEVICEID&token=$TOKEN" |
   grep --line-buffered -v '3:::{"jsonrpc":"2.0","method":"device_connected"' |
   grep --line-buffered -v '3:::{"jsonrpc":"2.0","id":3,"result":"ok"}' |
