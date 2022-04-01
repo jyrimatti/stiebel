d639690a262ac9627f15f5eaa2f93437d9169e1a
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2022-04-01 21:39:16 +0300

    ...

--- ouman_read.sh
+++ ouman_read.sh
@@ -2,10 +2,13 @@
 set -eu
 
 object=$1
-characteristic=${3:-}
+getset=$2
+characteristic=${4:-}
 
 if [ "$characteristic" == "On" ]; then
   echo 1
-else
+elif [ "$getset" == "Get" ]; then
   /home/pi/stiebel/ouman_get.sh "$object" "$(cat /home/pi/ouman-user)" "$(cat /home/pi/ouman-pass)"
+else
+  echo 1
 fi
