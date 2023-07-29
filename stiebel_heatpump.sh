9a2b1f22a6bf5a177d069d8c72a67b7d1778a383
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-29 13:03:53 +0300

    nix deps

--- stiebel_heatpump.sh
+++ stiebel_heatpump.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i bash -I channel:nixos-23.05-small -p bc gnugrep gnused
+#! nix-shell --pure -i bash -I channel:nixos-23.05-small -p bc gnugrep gnused gawk findutils
 set -eu
 
 section=$1
