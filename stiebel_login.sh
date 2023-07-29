65f4715a66608185270b3bc4de88266c84539795
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-07-29 13:06:29 +0300

    nix deps

--- stiebel_login.sh
+++ stiebel_login.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep STIEBEL_USER --keep STIEBEL_PASSWORD --keep STIEBEL_HOST -i bash -I channel:nixos-23.05-small -p curl flock
+#! nix-shell --pure --keep STIEBEL_USER --keep STIEBEL_PASSWORD --keep STIEBEL_HOST -i bash -I channel:nixos-23.05-small -p curl flock findutils
 set -eu
 
 mkdir -p "/tmp/stiebel-$USER"
