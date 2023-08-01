31ee2ba0e970b3fd62a7b65ef3a6f3718b73c2b8
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-08-01 14:32:40 +0300

    rsync limit

--- stiebel_rsync.sh
+++ stiebel_rsync.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i bash -I channel:nixos-23.05-small -p rsync sqlite
+#! nix-shell --pure -i bash -I channel:nixos-23.05-small -p rsync sqlite openssh
 set -eu
 
 remoteuser=$1
@@ -9,7 +9,7 @@ remotefile=$3
 foo() {
     echo 'PRAGMA locking_mode = EXCLUSIVE;'
     echo 'BEGIN EXCLUSIVE;'
-    rsync -avzq -e "ssh -qo StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ./stiebel.db "$remoteuser@$remotehost:$remotefile" 1>&2
+    rsync -avzq --bwlimit=1000 -e "ssh -qo StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ./stiebel.db "$remoteuser@$remotehost:$remotefile" 1>&2
     echo 'COMMIT;'
     echo '.quit'
 }
