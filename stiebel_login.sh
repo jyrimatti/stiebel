f35d4f3b5e6ba5260de87583a2dd4a3473dfe21c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-09-25 22:10:40 +0300

    optimize curls

--- stiebel_login.sh
+++ stiebel_login.sh
@@ -4,7 +4,14 @@ set -eu
 
 login() {
     test -e "/tmp/stiebel-$USER" || mkdir -p "/tmp/stiebel-$USER"
-    flock "/tmp/stiebel-$USER/lock-login" curl -4 --silent --show-error --connect-timeout 30 -d "make=send&user=$STIEBEL_USER&pass=$STIEBEL_PASSWORD" --cookie-jar "/tmp/stiebel-$USER/cookies" http://$STIEBEL_HOST/?s=0 > /dev/null
+    # -i and --no-buffer to kill processing after cookie.
+    flock "/tmp/stiebel-$USER/lock-login" curl -i -4 --no-buffer --silent -d "make=send&user=$STIEBEL_USER&pass=$STIEBEL_PASSWORD" --cookie-jar "/tmp/stiebel-$USER/cookies" http://$STIEBEL_HOST/?s=0 | {
+        while read -r header; do
+            case $header in "Set-Cookie"*)
+                exit 0;; # stop when cookie received
+            esac
+        done
+    }
 }
 
 if [ ! -f "/tmp/stiebel-$USER/cookies" ]; then
