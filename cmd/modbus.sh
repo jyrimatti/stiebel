bbd2d9404374150d07d5a29998f2db914f290a81
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-10-10 17:01:41 +0300

    modbus edit

--- cmd/modbus.sh
+++ cmd/modbus.sh
@@ -4,28 +4,32 @@ set -eu
 
 object=$1
 getset=$2
+value=${4:-}
 
 . ./stiebel_env.sh
 
 . ./stiebel_objects.sh "$object"
 
-if [ "$getset" = "Get" ]; then
-  # ISG addresses are 1 based.
-  OBJECTID="$(echo "$OBJECTID" | cut -d@ -f 1)@$(echo "$OBJECTID" | cut -d@ -f 2 | sed 's/$/-1/' | bc)"
+# ISG addresses are 1 based.
+OBJECTID="$(echo "$OBJECTID" | cut -d@ -f 1)@$(echo "$OBJECTID" | cut -d@ -f 2 | sed 's/$/-1/' | bc)"
 
+if [ "$getset" = "Get" ]; then
   ret=$(modbus "$STIEBEL_HOST" "$OBJECTID" | grep -v 'Parsed 0 registers definitions' | cut -d' ' -f2)
-  if [ "$ret" = "Invalid" ]; then
-    echo "$OBJECTID: Invalid address" >&2
-    exit 1
-  elif [ "$ret" = "32768" ]; then
-    # The substitute value "32768 (0x8000H)" is issued for unavailable objects.
-    echo "$OBJECTID: Unavailable object" >&2
-    exit 2
-  elif [ "$ret" = "36864" ]; then
-    echo "$OBJECTID: OFF via 0x9000" >&2
-    exit 2
-  fi
   echo "$ret" | sed "s/$/*$MULTIPLIER/" | bc
+elif [ "$getset" = "Set" ]; then
+  ret=$(modbus "$STIEBEL_HOST" "$OBJECTID"="$(echo "$value" | sed "s/$/\/$MULTIPLIER/" | bc)" | grep -v 'Parsed 0 registers definitions' | cut -d' ' -f2)
 else
   exit 1
+fi
+
+if [ "$ret" = "Invalid" ]; then
+  echo "$OBJECTID: Invalid address" >&2
+  exit 1
+elif [ "$ret" = "32768" ]; then
+  # The substitute value "32768 (0x8000H)" is issued for unavailable objects.
+  echo "$OBJECTID: Unavailable object" >&2
+  exit 2
+elif [ "$ret" = "36864" ]; then
+  echo "$OBJECTID: OFF via 0x9000" >&2
+  exit 2
 fi
