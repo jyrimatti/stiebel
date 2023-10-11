38fdf875b328d29ce9aff709f4310a4ebaafd8a1
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-10-11 20:57:38 +0300

    dynamic temp

---
+++ cmd/dynamic_temp.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure --keep PRICE -i dash -I channel:nixos-23.05-small -p nix dash bc curl cacert gnused
+set -eu
+
+getset="$1"
+
+currentPrice="$(curl -s 'https://spot.lahteenmaki.net/current.csv?tax=24' | sed 's/.*,//g')"
+price="${PRICE:-$currentPrice}"
+
+targetTemp="$(echo "-5.5 * l($price - 0.2)/l(10) + 25" | bc -l)"
+
+# 21 degrees is a suitable "normal temperature" value for my setup
+
+if [ "$getset" = "Set" ]; then
+  ./cmd/modbus.sh COMFORT_TEMPERATURE_HC1 Set '' '' "$targetTemp"
+  ./cmd/modbus.sh ECO_TEMPERATURE_HC1 Set '' '' "$targetTemp"
+else
+  echo "$targetTemp"
+fi
