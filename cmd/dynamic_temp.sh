e589dd6eec428184aebb817caebc5ad316c4fa9c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-11-20 20:35:50 +0200

    improve dynamic temp

--- cmd/dynamic_temp.sh
+++ cmd/dynamic_temp.sh
@@ -18,18 +18,23 @@ else
   next="${NEXT_PRICE:-$nextPrice}"
 
   # some suitable logarithmic curve
-  targetTemp="$(echo "-5.5 * l($price + 0.5)/l(10) + 23" | bc -l)"
+  targetTemp="$(echo "-10 * l($price + 1.5)/l(10) + 27" | bc -l)"
 
   if [ "$(echo "$price + $next <= 3" | bc -l)" = "1" ]; then
     # if now and next hour is cheap, keep normal temperature
     effectiveTemp="$normalTemp"
   elif [ "$(echo "$price < $next" | bc -l)" = "1" ]; then
     # next hour will be more expensive, so preheat if it's not too expensive.
-    # Pre-heat more the more expensive the next hour is.
-    effectiveTemp="$(echo "$targetTemp + ($next-$price)/5" | bc -l)"
+    if [ "$(echo "$price > $targetTemp" | bc -l)" = "1" ]; then
+      # ugh, expensive, don't preheat
+      effectiveTemp="$targetTemp"
+    else
+      # pre-heat more the more expensive the next hour is.
+      effectiveTemp="$(echo "$targetTemp + ($next-$price)/5" | bc -l)"
+    fi
   else
-    # otherwise next hour will be cheaper, so don't preheat
-    effectiveTemp="$targetTemp"
+    # otherwise next hour will be cheaper, so heat less
+    effectiveTemp="$(echo "$targetTemp + ($next-$price)/5" | bc -l)"
     if [ "$(echo "$effectiveTemp > $normalTemp" | bc -l)" = "1" ]; then
       # Math.min($effectiveTemp, $normalTemp)
       effectiveTemp="$normalTemp"
@@ -44,7 +49,16 @@ fi
 
 if [ "$getset" = "Set" ]; then
   ./cmd/modbus.sh COMFORT_TEMPERATURE_HC1 Set '' '' "$effectiveTemp"
-  ./cmd/modbus.sh ECO_TEMPERATURE_HC1 Set '' '' "$effectiveTemp"
+  ./cmd/modbus.sh ECO_TEMPERATURE_HC1     Set '' '' "$effectiveTemp"
+  if [ "$(echo "$effectiveTemp < $normalTemp" | bc -l)" = "1" ]; then
+    # have to lower both circuits, otherwise buffer will still heat up more
+    ./cmd/modbus.sh COMFORT_TEMPERATURE_HC2 Set '' '' "$effectiveTemp"
+    ./cmd/modbus.sh ECO_TEMPERATURE_HC2     Set '' '' "$effectiveTemp"
+  else
+    # but don't raise actual circuit above normal room temperature
+    ./cmd/modbus.sh COMFORT_TEMPERATURE_HC2 Set '' '' "$normalTemp"
+    ./cmd/modbus.sh ECO_TEMPERATURE_HC2     Set '' '' "$normalTemp"
+  fi
 else
   echo "$effectiveTemp" >&2
   echo 0
