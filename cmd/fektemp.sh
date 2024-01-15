9a97f84730489ecbc01c53bac8e650f2d4c44d55
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-15 23:59:03 +0200

    optimize

--- cmd/fektemp.sh
+++ cmd/fektemp.sh
@@ -2,6 +2,4 @@
 #! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix gnused dash
 set -eu
 
-. ./stiebel_env.sh
-
-./stiebel_get.sh 1,0 | ./stiebel_system.sh "ROOM TEMPERATURE" "ACTUAL TEMPERATURE FEK" | sed 's/.* //' | sed 's/[^.0-9].*//' | sed 's/^[.][0.9]*$/0\0/' | sed 's/^[^.]*$/\0.0/'
+./cmd/modbus.sh ACTUAL_TEMPERATURE_FEK
