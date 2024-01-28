31c82f9d72271d5b27ab22c36da3dac2e6acefaa
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-28 18:35:01 +0200

    read temp with modbus

--- cmd/fektemp.sh
+++ cmd/fektemp.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix gnused dash
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash
 set -eu
 
 ./cmd/modbus.sh ACTUAL_TEMPERATURE_FEK Get
--- cmd/temp.sh
+++ cmd/temp.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix gnused dash
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash
 set -eu
 
-. ./stiebel_env.sh
-
-./stiebel_get.sh 1,0 | ./stiebel_system.sh "HEATING" "OUTSIDE TEMPERATURE" | sed 's/.* //' | sed 's/[^-.0-9].*//' | sed 's/^[.][0.9]*$/0\0/' | sed 's/^[^.]*$/\0.0/'
+./cmd/modbus.sh OUTSIDE_TEMPERATURE Get
