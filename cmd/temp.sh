3570aec7176bf9f2bd94b7e6888597d73e558b52
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-02-06 22:38:07 +0200

    modbus doesn't work for this...

--- cmd/temp.sh
+++ cmd/temp.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix gnused dash
 set -eu
 
-./cmd/modbus.sh OUTSIDE_TEMPERATURE Get
+. ./stiebel_env.sh
+
+./stiebel_get.sh 1,0 | ./stiebel_system.sh "HEATING" "OUTSIDE TEMPERATURE" | sed 's/.* //' | sed 's/[^-.0-9].*//' | sed 's/^[.][0.9]*$/0\0/' | sed 's/^[^.]*$/\0.0/'
