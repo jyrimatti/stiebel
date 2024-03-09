6d6ae892a772b959ee535298c0b43fe705409494
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-09 13:51:23 +0200

    fix locale warnings

--- stiebel_collect2db.sh
+++ stiebel_collect2db.sh
@@ -2,6 +2,8 @@
 #! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix dash sqlite parallel coreutils gnused curl cacert flock bc "pkgs.callPackage ./modbus_cli.nix {}"
 set -eu
 
+export LC_ALL=C # "fix" Nix Perl locale warnings
+
 # echo '.tables' | sqlite3 stiebel.db | tr ' ' '\n' | grep '[a-zA-Z]' | xargs -I{} dash -c 'echo -n "{} \t" && { echo "select measurement from {}" | sqlite3 stiebel.db; }'
 
 dash ./stiebel_collect.sh | sqlite3 -cmd ".timeout 90000" ./stiebel.db
