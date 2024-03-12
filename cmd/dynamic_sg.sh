5021a7e9a36c5c9df82f710d56f3d33fea28a6a7
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-12 20:28:14 +0200

    dynamic sg

---
+++ cmd/dynamic_sg.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure --keep PRICE --keep NEXT_PRICE --keep CURRENT_ROOM_TEMP --keep NIGHT_DELTA -i dash -I channel:nixos-23.11-small -p nix dash jq flock bc curl cacert gnused "pkgs.callPackage ./modbus_cli.nix {}"
+set -eu
+
+getset="${1:-}"
+
+limit=-3000
+
+power="$(cd ../homewizard && dash ./cmd/data.sh active_power_w;)"
+
+accelerate="$(echo "$power < $limit" | bc)"
+
+if [ "$getset" = "Set" ]; then
+  if [ "$accelerate" = 1 ]; then
+    dash ./cmd/sg_accelerated.sh Set
+  else
+    dash ./cmd/sg_standard.sh Set
+  fi
+else
+  echo "$accelerate"
+fi
