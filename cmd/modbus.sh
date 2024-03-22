6807ab249b3a4ac5ea3f1027c0c73227f485c30e
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-22 23:45:59 +0200

    try custom modbus cli

--- cmd/modbus.sh
+++ cmd/modbus.sh
@@ -6,23 +6,43 @@ object=$1
 getset=$2
 value=${5:-}
 
-if [ -z "${IN_NIX_SHELL:-}" ]; then
-  cmd="modbus -S"
-else
-  cmd="python ./modbus_cli_optimized.sh"
-fi
-
 . ./stiebel_env.sh
 
 . ./stiebel_objects.sh "$object"
 
+#if [ -z "${IN_NIX_SHELL:-}" ]; then
+#  cmd="modbus -S"
+#else
+#  cmd="python ./modbus_cli_optimized.sh"
+#fi
+case $OBJECTID in
+  h*)
+    fcode=3
+    ;;
+  i*)
+    fcode=4
+    ;;
+esac;
+register="${OBJECTID%%/*}"
+register="$((${register##*@} - 1))" # ISG addresses are 1 based.
+case ${OBJECTID##*/} in
+  h)
+    type=int16
+    ;;
+  *)
+    type=uint16
+    ;;
+esac
+
 # ISG addresses are 1 based.
-OBJECTID="$(echo "$OBJECTID" | cut -d@ -f 1)@$(echo "$OBJECTID" | cut -d@ -f 2 | cut -d/ -f 1 | sed 's/$/-1/' | bc)$(echo "$OBJECTID" | sed 's/[a-z]@[0-9]*//')"
+#OBJECTID="$(echo "$OBJECTID" | cut -d@ -f 1)@$(echo "$OBJECTID" | cut -d@ -f 2 | cut -d/ -f 1 | sed 's/$/-1/' | bc)$(echo "$OBJECTID" | sed 's/[a-z]@[0-9]*//')"
 
 if [ "$getset" = "Get" ]; then
-  ret=$($cmd "$STIEBEL_HOST" "$OBJECTID")
+  #ret=$($cmd "$STIEBEL_HOST" "$OBJECTID")
+  ret=$(dash ./modbus.sh/modbus.sh -m "$MULTIPLIER" "$STIEBEL_HOST" "$fcode" "$register" "$type")
 elif [ "$getset" = "Set" ]; then
-  $cmd "$STIEBEL_HOST" "$OBJECTID"="$(echo "$value" | sed "s/$/\/$MULTIPLIER/" | bc)"
+  #$cmd "$STIEBEL_HOST" "$OBJECTID"="$(echo "$value" | sed "s/$/\/$MULTIPLIER/" | bc)"
+  dash ./modbus.sh/modbus.sh -m "$MULTIPLIER" "$STIEBEL_HOST" "$fcode" "$register" "$type" "$value"
   ret=1
 else
   exit 1
@@ -39,5 +59,5 @@ elif [ "$ret" = "36864" ] || [ "$ret" = "-28672" ]; then
   echo "$OBJECTID: OFF via 0x9000" >&2
   exit 2
 else
-  echo "$ret" | sed "s/$/*$MULTIPLIER/" | bc | sed 's/^\./0./' | sed 's/^-\./-0./' 
+  echo "$ret" #| sed "s/$/*$MULTIPLIER/" | bc | sed 's/^\./0./' | sed 's/^-\./-0./' 
 fi
