28e361b31941128bf2998ce781a529773479d295
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-22 23:56:36 +0200

    add dep

--- cmd/modbus.sh
+++ cmd/modbus.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix gnused dash bc "pkgs.callPackage ./modbus_cli.nix {}"
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p nix gnused dash bc netcat "pkgs.callPackage ./modbus_cli.nix {}"
 set -eu
 
 object=$1
