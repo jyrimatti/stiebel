794cde9f7b28e3574da7e637d8352b90ff42db06
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-04-10 07:25:25 +0300

    ...

--- cmd/sg_standard.sh
+++ cmd/sg_standard.sh
@@ -5,13 +5,19 @@ set -eu
 getset="${1:-}"
 
 if [ "$getset" = "Set" ]; then
-  if [ "$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Get)" != 0 ]; then
-    response="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Set '' '' 0)"
+  ret1="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Get)"
+  ret2="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Get)"
+  if [ "$ret1" = 0 ] && [ "$ret2" = 0 ]; then
+    echo 1
+  else
+    if [ "$ret1" != 0 ]; then
+      response="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Set '' '' 0)"
+    fi
+    if [ "$ret2" != 0 ]; then
+      response="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Set '' '' 0)"
+    fi
+    echo 0
   fi
-  if [ "$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Get)" != 0 ]; then
-    response="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Set '' '' 0)"
-  fi
-  echo 0
 else
   if [ "$(dash ./cmd/modbus.sh SG_READY_OPERATING_STATE Get)" = 2 ]; then
     echo 1
