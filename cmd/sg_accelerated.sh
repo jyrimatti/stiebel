477f9c5df295202bc402a5cd2d3bce56ff27bd21
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-01 10:17:06 +0300

    possibility to switch off sg acc/max

--- cmd/sg_accelerated.sh
+++ cmd/sg_accelerated.sh
@@ -3,20 +3,31 @@
 set -eu
 
 getset="${1:-}"
+value="${4:-}"
+if [ "$value" = "true" ] || [ "$value" = "1" ]; then
+  value="1";
+else
+  value="0";
+fi
 
 if [ "$getset" = "Set" ]; then
-  ret1="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Get)"
-  ret2="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Get)"
-  if [ "$ret1" = 1 ] && [ "$ret2" = 0 ]; then
-    echo 1
-  else
-    if [ "$ret1" != 1 ]; then
-      response="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Set '' '' 1)"
-    fi
-    if [ "$ret2" != 0 ]; then
-      response="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Set '' '' 0)"
-    fi
+  if [ "$value" = 0 ]; then
+    response="$(dash ./cmd/sg_standard.sh Set)"
     echo 0
+  else
+    ret1="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Get)"
+    ret2="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Get)"
+    if [ "$ret1" = 1 ] && [ "$ret2" = 0 ]; then
+      echo 1
+    else
+      if [ "$ret1" != 1 ]; then
+        response="$(dash ./cmd/modbus.sh SG_READY_INPUT_1 Set '' '' 1)"
+      fi
+      if [ "$ret2" != 0 ]; then
+        response="$(dash ./cmd/modbus.sh SG_READY_INPUT_2 Set '' '' 0)"
+      fi
+      echo 0
+    fi
   fi
 else
   if [ "$(dash ./cmd/modbus.sh SG_READY_OPERATING_STATE Get)" = 3 ]; then
