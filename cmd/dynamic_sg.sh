c77aaa112257e2846e46a784a9fa05cf30106516
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-05-31 21:43:55 +0300

    value

--- cmd/dynamic_sg.sh
+++ cmd/dynamic_sg.sh
@@ -3,6 +3,7 @@
 set -eu
 
 getset="${1:-}"
+value="${4:-}"
 
 powerLimit=-3000
 dhwTempLimit=57
@@ -15,7 +16,7 @@ tempNotMax="$(echo "$dhwTemp < $dhwTempLimit" | bc)"
 
 accelerate="$([ "$powerAvailable" = 1 ] && [ "$tempNotMax" = 1 ] && echo 1 || echo 0)"
 if [ "$getset" = "Set" ]; then
-  if [ "$accelerate" = 1 ]; then
+  if [ "$accelerate" = 1 ] && [ "$value" = 1 ]; then
     response="$(dash ./cmd/sg_accelerated.sh Set)"
   else
     response="$(dash ./cmd/sg_standard.sh Set)"
