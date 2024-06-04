495898326e38884d77dd5d93f9a0f7a17699c700
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-04 20:46:01 +0300

    rework flock

--- stiebel_get.sh
+++ stiebel_get.sh
@@ -6,31 +6,36 @@ page="$1"
 
 DIR="${XDG_RUNTIME_DIR:-/tmp}/stiebel"
 
+test -e "$DIR" || mkdir -p "$DIR"
+
 # cache page fetching since Stiebel sucks and only allows one request concurrently, which takes ~2 seconds...
 
-fetch() {
-    relogin="${1:-0}"
-    cookies="$(dash ./stiebel_login.sh "$relogin")"
+(
+    flock 8
 
-    test -e "$DIR" || mkdir -p "$DIR"
-    flock "$DIR/lock-$page" curl -4 --silent --show-error -L -b "$cookies" http://$STIEBEL_HOST/?s=$page | sed 's/"OFF"/"0"/' | sed 's/"ON"/"1"/'
-}
+    fetch() {
+        relogin="${1:-0}"
+        cookies="$(dash ./stiebel_login.sh "$relogin")"
 
-doFetch() {
-    response="$(fetch)"
-    if { echo "$response" | grep -q 'User name:'; }; then
-        response="$(fetch 1)"
-    fi
-    echo "$response" > "$DIR/$page"
-}
+        curl -4 --silent --show-error -L -b "$cookies" http://$STIEBEL_HOST/?s=$page | sed 's/"OFF"/"0"/' | sed 's/"ON"/"1"/'
+    }
 
-if [ ! -f "$DIR/$page" ] || [ ! -s "$DIR/$page" ]; then
-    doFetch
-else
-    for i in $(find "$DIR/$page" -mmin +1); do
+    doFetch() {
+        response="$(fetch)"
+        if { echo "$response" | grep -q 'User name:'; }; then
+            response="$(fetch 1)"
+        fi
+        echo "$response" > "$DIR/$page"
+    }
+
+    if [ ! -f "$DIR/$page" ] || [ ! -s "$DIR/$page" ]; then
         doFetch
-    done
-fi
+    else
+        for i in $(find "$DIR/$page" -mmin +1); do
+            doFetch
+        done
+    fi
+) 8> "$DIR/lock-$page"
 
 if [ ! -s "$DIR/$page" ]; then
     echo "Stiebel page $page is empty" >&2
