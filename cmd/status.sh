b1717a2c82d6c6a077042d99e8a03cb5c9045539
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-04 21:00:29 +0300

    hmph

--- cmd/status.sh
+++ cmd/status.sh
@@ -19,4 +19,6 @@ value="$1"
 
 . ./stiebel_env.sh
 
-dash ./stiebel_get.sh 2,0 | { grep -q ">$value<" && echo 1; } || echo 0
+dash ./stiebel_get.sh 2,0 | {
+    grep -q ">$value<" && echo 1 || echo 0
+}
