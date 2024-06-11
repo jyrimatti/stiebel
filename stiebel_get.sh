134b94d99bc5d8b818be5a089801a0c81a1a0dde
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-06-11 23:37:22 +0300

    unify get and data collection

--- stiebel_get.sh
+++ stiebel_get.sh
@@ -10,32 +10,28 @@ test -e "$DIR" || mkdir -p "$DIR"
 
 # cache page fetching since Stiebel sucks and only allows one request concurrently, which takes ~2 seconds...
 
-(
-    flock 8
+fetch() {
+    relogin="${1:-0}"
+    cookies="$(dash ./stiebel_login.sh "$relogin")"
 
-    fetch() {
-        relogin="${1:-0}"
-        cookies="$(dash ./stiebel_login.sh "$relogin")"
+    dash ./curl_cached.sh http://$STIEBEL_HOST/?s=$page -b "$cookies" | sed 's/"OFF"/"0"/' | sed 's/"ON"/"1"/'
+}
 
-        curl -4 --silent --show-error -L -b "$cookies" http://$STIEBEL_HOST/?s=$page | sed 's/"OFF"/"0"/' | sed 's/"ON"/"1"/'
-    }
-
-    doFetch() {
-        response="$(fetch)"
-        if { echo "$response" | grep -q 'User name:'; }; then
-            response="$(fetch 1)"
-        fi
-        echo "$response" > "$DIR/$page"
-    }
-
-    if [ ! -f "$DIR/$page" ] || [ ! -s "$DIR/$page" ]; then
-        doFetch
-    else
-        for i in $(find "$DIR" -name "$page" -mmin +1); do
-            doFetch
-        done
+doFetch() {
+    response="$(fetch)"
+    if { echo "$response" | grep -q 'User name:'; }; then
+        response="$(fetch 1)"
     fi
-) 8> "$DIR/lock-$page"
+    echo "$response" > "$DIR/$page"
+}
+
+if [ ! -f "$DIR/$page" ] || [ ! -s "$DIR/$page" ]; then
+    doFetch
+else
+    for i in $(find "$DIR" -name "$page" -mmin +1); do
+        doFetch
+    done
+fi
 
 if [ ! -s "$DIR/$page" ]; then
     echo "Stiebel page $page is empty" >&2
