6cb00c57d1a323b28a2211f8d563e657845f57fb
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-10-12 16:23:52 +0300

    increase max temp a bit

--- cmd/dynamic_temp.sh
+++ cmd/dynamic_temp.sh
@@ -7,7 +7,7 @@ getset="${1:-}"
 targetRoomTemp=21               # target room temperature
 targetPumpTemp=19               # value that is "enough" for the pump to keep room temperature near targetRoomTemp
 minTemp=12                      # don't try below this
-maxTemp=25                      # don't try above this
+maxTemp=27                      # don't try above this
 nightDelta=${NIGHT_DELTA:-1.69} # reduce price by this much during night time
 nightStart=22                   # night time starts at this hour
 nightEnd=7                      # night time ends at this hour
@@ -34,7 +34,7 @@ else
   fi
 
   # some suitable logarithmic curve
-  targetTemp="$(echo "-10 * l($price + 1.5)/l(10) + 27" | bc -l)"
+  targetTemp="$(echo "-10 * l($price + 1.5)/l(10) + $maxTemp" | bc -l)"
 
   # next hour will be more expensive -> pre-heat more the more expensive the next hour is.
   # next hour will be cheaper -> heat less.
@@ -75,6 +75,8 @@ if [ "$getset" = "Set" ]; then
       response="$(dash ./cmd/modbus.sh ECO_TEMPERATURE_HC2 Set '' '' "$effectiveTemp")"
     fi
   fi
+elif [ "$getset" = "Get" ]; then
+  echo 0
+else
+  echo "$effectiveTemp"
 fi
-
-echo 0
--- cmd/dynamic_temp_test.sh
+++ cmd/dynamic_temp_test.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p nix dash bc curl cacert gnused
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep LD_LIBRARY_PATH --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-24.05-small -p nix dash bc curl cacert gnused
 set -eu
 
 echo "price room_21 room_20 room_19 room_18"
 for price in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
   echo -n "$price"
   for room in 21 20 19 18; do
-    val=$(PRICE="$price" NEXT_PRICE="$price" CURRENT_ROOM_TEMP="$room" dash ./cmd/dynamic_temp.sh Get 2>&1 1>/dev/null | tr '.' ',')
+    val=$(PRICE="$price" NEXT_PRICE="$price" CURRENT_ROOM_TEMP="$room" dash ./cmd/dynamic_temp.sh | tr '.' ',')
     echo -n " $val"
   done
   echo ""
