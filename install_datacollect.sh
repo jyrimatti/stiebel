2493dae8c9e15eaa9ca66560d6ddeb5f45c5959d
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-02-19 18:15:33 +0200

    explicit sh

--- install_datacollect.sh
+++ install_datacollect.sh
@@ -19,7 +19,7 @@ After=syslog.target network.target
 
 [Service]
 Type=oneshot
-ExecStart=/bin/sh -c '. /etc/profile.d/nix.sh; LD_LIBRARY_PATH="$(find /nix/store -maxdepth 1 -name '*nss-mdns*' -type d)/lib" PATH=$HOME/.local/nix-override:\$PATH $scriptdir/${service}_collect2db.sh'
+ExecStart=/bin/sh -c '. /etc/profile.d/nix.sh; LD_LIBRARY_PATH="$(find /nix/store -maxdepth 1 -name '*nss-mdns*' -type d)/lib" PATH=$HOME/.local/nix-override:\$PATH sh $scriptdir/${service}_collect2db.sh'
 CPUWeight=128
 
 ProtectSystem=strict
